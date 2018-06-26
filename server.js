cf1a97136fe9cfcc92ea205b9804212ce1e3e75e
ccareyncsu <ccarey@ncsu.edu>
2018-06-26 13:05:52 -0600

    added error messages

--- server.js
+++ server.js
@@ -27,17 +27,28 @@ app.get('/api/notes', (req, res) => {
   }
 });
 
-// app.get('/api/notes/:id', (req, res) => {
-//   res.id.json(data[req.params.id]);
-// });
-
 app.get('/api/notes/:id', (req, res) =>{
   const id = req.params.id;
-  const note = data.find(item => item.id === Number(id));
-
-  res.json(note);
+  res.json(data.find(item => item.id === Number(id)));
 });
 
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
 
 app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
