1f792cec5b0c51f0a3f1e63887e54eeb4b5e9afe
ccareyncsu <ccarey@ncsu.edu>
2018-06-27 14:41:35 -0600

    post and delete method added

--- router/notes.router.js
+++ router/notes.router.js
@@ -58,11 +58,41 @@ router.put('/notes/:id', (req, res, next) => {
       next();
     }
   });
+});      
+
+// Post (insert) an item
+router.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
 });
 
-
-router.get('/boom', (req, res, next) => {
-  throw new Error('Boom!!');
+router.delete('/notes/:id', (req, res, next) => {
+  const {id} = req.params;
+  notes.delete(id, (err) => {
+    if (err) {
+      return next(err);
+    }else{
+      res.sendStatus(204);
+    }
+  });
 });
 
 module.exports = router;
